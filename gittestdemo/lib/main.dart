c334d2118ead591911f3931dfa282f09fa04960e
jami000 <pnhat0914@gmail.com>
2022-02-17 17:00:51 +0700

    main commit 3

--- gittestdemo/lib/main.dart
+++ gittestdemo/lib/main.dart
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Hello world B1',
+              'Day la code cua branch main',
               style: TextStyle(fontSize: 40, color: Colors.red),
             ),
           ],
